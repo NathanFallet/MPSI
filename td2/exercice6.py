aea152c2e7325f9492e8f5a0b239c1515b4c4f9a
Nathan Fallet <contact@nathanfallet.me>
2020-09-24 14:52:39 +0200

    TD 2 - Ex 6

---
+++ td2/exercice6.py
@@ -0,0 +1,36 @@
+# Question 1
+
+# On definit la fonction qui fait la conversion
+def conversion(h, m, s):
+    # On converti
+    return s + m * 60 + h * 3600
+
+# On test notre fonction
+# Ceci devrait afficher 5415
+print(conversion(1, 30, 15))
+
+# Question 2
+
+# On demande les deux horaires
+
+# La premiere
+print("Horaire 1 :")
+h1 = input("h = ")
+m1 = input("m = ")
+s1 = input("s = ")
+
+# La deuxieme
+print("Horaire 2 :")
+h2 = input("h = ")
+m2 = input("m = ")
+s2 = input("s = ")
+
+# On converti
+conversion1 = conversion(h1, m1, s1)
+conversion2 = conversion(h2, m2, s2)
+
+# On fait la difference
+difference = abs(conversion1 - conversion2)
+
+# On affiche le resultat
+print("La duree en secondes entre les deux horaires est " + str(difference) + " s")
