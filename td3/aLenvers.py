5b6d594bf9be0c4fa2cae68c6c21ce72a67563d5
H2WO4 <54752489+H2WO4@users.noreply.github.com>
2020-10-14 17:27:49 +0200

    Exercice 2 et 3

---
+++ td3/aLenvers.py
@@ -0,0 +1,22 @@
+# On demande un nombre
+n = int(input("Entrez un nombre : "))
+
+# On initialise une liste
+list = []
+#On répète tant que n est different 
+while n != 0:
+    # On ajoute le chiffre à la liste
+    list.append(n % 10)
+    # On supprime le chiffre des unités en le divisant par 10
+    n //= 10 # n = n // 10
+
+# On initialise une variable
+nombre = 0
+
+# On parcours la liste des chiffres
+for j in range(len(list)):
+    # Pour chaque nombre, on le multiplie par la puissance de 10 adéquate
+    nombre += list[j] * (10 ** (len(list) - j - 1))
+
+# On affiche le résultat
+print(nombre)
