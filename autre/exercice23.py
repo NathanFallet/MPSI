6b6df1bc802f713422ee892a281bac78f2d42c7c
H2WO4 <54752489+H2WO4@users.noreply.github.com>
2020-10-16 12:49:45 +0200

    Ajout de 2 programmes autres

---
+++ autre/exercice23.py
@@ -0,0 +1,54 @@
+""" Recursive Function """
+
+def getParent(sequence, units, number):
+    """ Finds all of a number's parents """
+
+    # On parcours toute la liste
+    for j in range(len(sequence)):
+        # Pour chaque nombre supérieur à 1, on lui retire n, compris entre n et j - 1
+        for i in range(1, (sequence[j] // 2) + 1):
+            # On place n dans la liste, à gauche de j
+            sequenceCopy = sequence.copy()
+            sequenceCopy[j] -= i
+            sequenceCopy.append(i)
+
+            # On fait une première récursion
+            if not getParent(sequenceCopy, units, number):
+                return False
+
+            # On place n dans la liste, à droite de j
+            sequenceCopy2 = sequence.copy()
+            sequenceCopy2[j] -= i
+            sequenceCopy2.insert(j-1, i)
+
+            # On fait une deuxième récursion
+            if not getParent(sequenceCopy2, units, number):
+                return False
+
+    # On teste si la sequence en question est divisible par le nombre actuel
+    if int("".join([str(j) for j in sequence]) + str(units)) % number == 0:
+        return True
+    return False
+
+
+
+""" Main loop """
+
+# Definition du liste pour l'ensemble de sortie
+n = []
+
+# Boucle testant tout les cas
+for i in range(10, 100):
+    # On sépare chiffre des unités et des dizaines
+    units = i % 10
+    tens = (i - units) // 10
+
+    # On lance la fonction récursive
+    parentsDivisibility = getParent([tens], units, i)
+
+    # Si aucun des nombres parents n'a renvoyé False, alors tous sont divisibles par i
+    if parentsDivisibility:
+        n.append(i)
+
+# On affiche la réponse
+print(n)
--- td4/trianglePascal.py
+++ td4/trianglePascal.py
@@ -5,13 +5,13 @@ n = int(input("Entrez un nombre : "))
 triangle = [[1]]
 # On lance la boucle
 for i in range(1, n+1):
-    # On ajoute une nouvelle liste pour cet étage, avec le premier élément initialisé
+    # On ajoute une nouvelle liste pour cet étage, avec le premier élément initialisé, car il est toujours 1
     triangle.append([1])
     # On effectue pour chaque valeur à ajouter
     for j in range(i - 1):
         # On calcule grace à la formule du triangle de Pascal
         triangle[i].append(triangle[i-1][j] + triangle[i-1][j+1])
-    # On rajoute le 1 de fin de ligne
+    # On rajoute le 1 de fin de la ligne
     triangle[i].append(1)
 
 # On affiche chaque ligne du triangle 1 à 1
